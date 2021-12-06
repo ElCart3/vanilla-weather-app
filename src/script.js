40e39c40762d76c6ce9c6dda387559bb3595b073
ElCart3 <elliejane.carter@gmail.com>
2021-12-06 21:31:36 +0000

    Edited styling

--- src/script.js
+++ src/script.js
@@ -26,7 +26,7 @@ function formatDate(timestamp) {
 function formatWeekday(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
 
